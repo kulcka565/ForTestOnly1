f4eb9ceab3cc7ffc0b863fd83c7456d580254569
kulcka565 <kulcka565@yandex.ru>
2024-09-29 21:49:24 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -53,3 +53,12 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
+class BigCard extends StatelessWidget {
+const BigCard({super.key, required this.pair});
+  @override
+  Widget build(BuildContext context) {
+  final theme=Theme.of(context);
+  return Card(color: theme.colorScheme.primary,
+  child: Padding(padding: const EdgeInsets.all(20),
+  child: Text(pair.asLowerCase),),);
+  }
